b13f81f887115dc27c1c3915c89cc8172ec4f612
SirPoppyThFatCat <samuel.demarchi07@gmail.com>
2026-04-24 10:18:51 +0100

    made a Ui. it doesn't really do anything

--- UserInterface.java
+++ UserInterface.java
@@ -1,3 +1,8 @@
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /** 
  * 
  * 
@@ -6,6 +11,16 @@
 
 public class UserInterface {
     public static void main(String[] args) {
-        
+        JFrame newFrame = new JFrame();
+        JPanel newPanel = new JPanel();
+        JButton newButton = new JButton("This does nothing and you will be happy with it");
+        newPanel.add(newButton);
+        newFrame.setContentPane(newPanel);
+        newFrame.setTitle("hahahahhahahahaha this is a virus");
+        newFrame.setSize(1000,1000);
+        newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        newFrame.setVisible(true);
+
+
     }
 }
