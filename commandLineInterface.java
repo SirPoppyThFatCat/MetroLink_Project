84de330ded9d46704e42bba76d735a4e9ee9062e
SirPoppyThFatCat <samuel.demarchi07@gmail.com>
2026-04-25 13:58:33 +0100

    comments and a compiled CLI file

--- commandLineInterface.java
+++ commandLineInterface.java
@@ -7,11 +7,14 @@ public class commandLineInterface {
     public static void main(String[] args) {
         Scanner routeInput = new Scanner(System.in);
         Scanner routeDestination = new Scanner(System.in);
+        // vars for the user to input the station
         System.out.println("Enter your starting station: ");
         String inputStation = routeInput.nextLine();
+
         System.out.println("Enter your destination station: ");
         String destinationStation = routeDestination.nextLine();
-        System.out.println("Your starting station is: " + inputStation + " and your destination station is: " + destinationStation);
-        System.out.println();
+
+        System.out.println("Your starting station is: " + inputStation + " and your destination station is: " + destinationStation); // temporary, sanity checking for myself
+        System.out.println("beans");
     }
 }
