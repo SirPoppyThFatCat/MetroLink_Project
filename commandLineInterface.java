a992715dda5656b17fdad5cc7ed82637b85f13c8
SirPoppyThFatCat <samuel.demarchi07@gmail.com>
2026-04-25 13:55:42 +0100

    working start and destination ask using Scanner class

--- commandLineInterface.java
+++ commandLineInterface.java
@@ -7,10 +7,11 @@ public class commandLineInterface {
     public static void main(String[] args) {
         Scanner routeInput = new Scanner(System.in);
         Scanner routeDestination = new Scanner(System.in);
-        System.out.println("Enter your start destination");
+        System.out.println("Enter your starting station: ");
         String inputStation = routeInput.nextLine();
-        String destinationStation = routeDestination.nextline();
-        System.out.println("Your input station is: " + inputStation + " yes");
+        System.out.println("Enter your destination station: ");
+        String destinationStation = routeDestination.nextLine();
+        System.out.println("Your starting station is: " + inputStation + " and your destination station is: " + destinationStation);
         System.out.println();
     }
 }
