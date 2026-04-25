cfaee42fb0a18a46f5c5032a7d3491d2adcbef2d
SirPoppyThFatCat <samuel.demarchi07@gmail.com>
2026-04-25 13:52:29 +0100

    added a destination station ask

--- commandLineInterface.java
+++ commandLineInterface.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 public class commandLineInterface {
     public static void main(String[] args) {
         Scanner routeInput = new Scanner(System.in);
+        Scanner routeDestination = new Scanner(System.in);
         System.out.println("Enter your start destination");
-        String name = routeInput.nextLine();
-
-        char inputStation
+        String inputStation = routeInput.nextLine();
+        String destinationStation = routeDestination.nextline();
+        System.out.println("Your input station is: " + inputStation + " yes");
+        System.out.println();
     }
 }
