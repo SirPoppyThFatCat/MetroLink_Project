6fc77d5758b5ab781918b2b72db71873da04cf77
SirPoppyThFatCat <samuel.demarchi07@gmail.com>
2026-04-25 21:02:42 +0100

    added a script to read through the CSV file? need to integrate it into the eventual checking algorithm. there's gotta be a library that does ts

--- commandLineInterface.java
+++ commandLineInterface.java
@@ -1,4 +1,7 @@
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 
@@ -15,5 +18,18 @@ public class commandLineInterface {
 
         System.out.println("Your starting station is: " + inputStation + " and your destination station is: " + destinationStation); // temporary, sanity checking for myself
         System.out.println("beans");
+    }
+}
+
+class stationScanner {
+    public static void main(String[] args) {
+        try (BufferedReader bufReader = new BufferedReader(new FileReader("Metrolink_times_linecolour(in).csv"))){
+            String line;
+            while ((line = bufReader.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch(IOException e) {
+            System.out.println("ahh shit the file doesn't work");
+        }
     }
 }
