6c349e43c94c5fc450a44cfd5f3294ef8da2d4af
SirPoppyThFatCat <samuel.demarchi07@gmail.com>
2026-04-27 20:34:13 +0100

    yeah this needs to be fully rewritten

--- stationScanner.java
+++ stationScanner.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.*;
+import javax.xml.crypto.Data;
 
 public class stationScanner {
 
@@ -20,9 +21,16 @@ public class stationScanner {
                 String[] values = line.split(",");
                 if(values.length>2){ //use for-each loop?
                 System.out.println(values[0].trim().toLowerCase().contains(commandLineInterface.InputStation));}//use switch case?
-//System.out.println(txt1.equals(txt2));  // true
+//System.out.println(txt1.equals(txt2)); use to compare if it's the same  // true
 
+/*int[][] myNumbers = { {1, 4, 2}, {3, 6, 8, 5, 2} };idk
 
+for (int row = 0; row < myNumbers.length; row++) {
+  for (int col = 0; col < myNumbers[row].length; col++) {
+    System.out.println("myNumbers[" + row + "][" + col + "] = " + myNumbers[row][col]);
+  }
+} */
+// adapt for the other array 
             }
         } catch (IOException e) {
             System.out.println("ahh shit the file doesn't work"); // need to redo the exemption in the future
