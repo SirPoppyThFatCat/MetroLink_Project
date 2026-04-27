2b4a4d6c055b6f846fd312e5cde5111dc3ceb4dd
SirPoppyThFatCat <samuel.demarchi07@gmail.com>
2026-04-27 10:07:48 +0100

    redoing part of the code to read the csv file into an array

--- stationScanner.java
+++ stationScanner.java
@@ -1,31 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.*;
 
 public class stationScanner {
     public static void main(String[] args) {
-        String path = "Metrolink_times_linecolour(in).csv";
-        int columnIndex = 0; // Index of the column to read
-        List<String> columnData = scanStations(path, columnIndex);
-        System.out.println(columnData);
+        int columnIndex = 2; // Index of the column to read
+        // List<String> columnData = scanStations(path, columnIndex);
+        // System.out.println(columnData);
+        scanStations();
     }
 
-    public static List<String> scanStations(String filePath, int columnIndex) {
-        List<String> columnData = new ArrayList<>();
-        //ArrayList<String> stationsData = new ArrayList<>(); to be used later. 
-        String toSplitBy = ",";
-        try (BufferedReader bufReader = Files.newBufferedReader(Paths.get(filePath))) {
-            String line;
+    public static void /* List<String> */ scanStations(/* String filePath, int columnIndex */) {
+        // List<String> columnData = new ArrayList<>();
+        // ArrayList<String> stationsData = new ArrayList<>(); to be used later.
+        // String toSplitBy = ",";
+        String path = "Metrolink_times_linecolour(in).csv";
+
+        try (BufferedReader bufReader = Files.newBufferedReader(Paths.get(path))) {
+            String line = "";
             while ((line = bufReader.readLine()) != null) {
-                String[] values = line.split(toSplitBy);
-                if (columnIndex < values.length) {
-                    columnData.add(values[columnIndex]);
-                }
+                // String[] values = line.split(toSplitBy);
+                // if (columnIndex < values.length) {
+                // columnData.add(values[columnIndex]);
+                System.out.println(line);
             }
         } catch (IOException e) {
             System.out.println("ahh shit the file doesn't work"); // need to redo the exemption in the future
         }
-        return columnData;
     }
+    // return columnData;
 }
