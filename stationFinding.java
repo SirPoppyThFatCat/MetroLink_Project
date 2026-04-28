bfb6fe83f6018c4043039623a9a5ada965e8597f
SirPoppyThFatCat <samuel.demarchi07@gmail.com>
2026-04-28 16:49:05 +0100

    got a buffered arraylist. modifying to check

--- stationFinding.java
+++ stationFinding.java
@@ -18,22 +18,34 @@ public class stationFinding {
     }
 
     static void searchStations() {
-        //parse into an arraylist for easier searching. 
-        System.out.println("i am a shape");
+        // parse into an arraylist for easier searching.
+        String csvSplittingValue = ",";
+        String line = "";
         // using bufferedReader here. checking sodding CSV file
+        //List<List<String>> dataList = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("Metrolink_times_linecolour(in).csv"))) {
-            String line;
-            while ((line = br.readLine()) != null){
-                System.out.println(line);
+
+            while ((line = br.readLine()) != null) {
+                //System.out.println(line);
+                String[] values = line.split(csvSplittingValue);
+                List<String> row = new ArrayList<>();
+                for (String value : values) {
+                    row.add(value);
+                }
+                //dataList.add(row);
             }
 
         } catch (IOException e) {
             System.out.println("the file is oer");
         }
+        for (List<String> row : dataList) {
+            System.out.println(row);
+        }
     }
 
     public static void main(String[] args) {
-        //chooseStations();
+        // chooseStations();
         searchStations();
 
-}}
+    }
+}
