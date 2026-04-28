a1dc83cc6ee43d4185f0ee44d9a92738d944abbd
SirPoppyThFatCat <samuel.demarchi07@gmail.com>
2026-04-28 19:45:04 +0100

    completely redoing the searching logic again. it was crap

--- stationFinding.java
+++ stationFinding.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import java.util.*;
 
 public class stationFinding {
@@ -11,45 +10,21 @@ public class stationFinding {
 
         System.out.println("enter the destination station: ");
         String destinationStation = myScanner.nextLine();
-        myScanner.close();
+        // myScanner.close();
 
         System.out.println(
                 "Your entry station is: " + inputStation + " and your destination station is: " + destinationStation);
-        }
-        static void searchForStation(){
+        searchForStation(inputStation);
 
-        // parse into an arraylist for easier searching.
-        String csvSplittingValue = ",";
-        String line = "";
-        // System.out.println("i am a shape");
-        // using bufferedReader here. checking sodding CSV file
-        // List<List<String>> dataList = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("Metrolink_times_linecolour(in).csv"))) {
-
-            while ((line = br.readLine()) != null) {
-                // System.out.println(line);
-                String[] values = line.split(csvSplittingValue);
-
-                List<String> row = new ArrayList<>();
-                row.add(values[0]);
-                // dataList.add(row);
-                if (row.get(0).equals(inputStation)) {
-                    System.out.println("yay station exists");
-                }
-
-            }
-
-        } catch (IOException e) {
-            System.out.println("the file is oer");
-        }
-        // for (List<String> row : dataList) {
-        // System.out.println(row.values[0]);
     }
-}
 
-public static void main(String[] args) {
-    chooseStations();
-    // searchStations();
+    static void searchForStation(String inputStation) {
 
-}
+      
+    }
+
+    public static void main(String[] args) {
+        chooseStations();
+
+    }
 }
