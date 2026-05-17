05554face13a4e161a0294e7c7464858e8029b98
SirPoppyThFatCat <samuel.demarchi07@gmail.com>
2026-05-17 20:56:37 +0100

    adding weights for the hashmap

--- GraphList.java
+++ GraphList.java
@@ -4,7 +4,15 @@ public class GraphList<Label> {
 
     // adjacency list container
     GraphNode headNode;
-    // Map<<c.fromStation, c.destinationStation>, Double> weights;
+    private Map<String, Double> weightMap = new HashMap<>(); // hashmap
+
+    private String makeKey(Label from, Label to) {
+        return from + "," + to;
+    }
+
+    public void storeWeight(Label from, Label to, double time) {
+        weightMap.put(makeKey(from, to), time);
+    }
 
     public class GraphNode {
         Label id; // station name
@@ -13,7 +21,7 @@ public class GraphList<Label> {
 
         public GraphNode(Label label) {
             this.id = label;
-            this.edges = new LinkedHashSet<GraphNode>();
+            this.edges = new LinkedHashSet<>();
         }
     }
 
@@ -26,44 +34,34 @@ public class GraphList<Label> {
     private GraphNode findNode(Label label) {
         GraphNode node = headNode;
         while (node != null) {
-            if (node.id == label)
+            if (node.id.equals(label))
                 return node;
             node = node.next;
         }
         return null;
     }
 
-    public void getOrCreateNode(Label label) {
-        node = findNode(label);
+    private GraphNode getOrCreateNode(Label label) {
+        GraphNode node = findNode(label);
+
         if (node == null) {
             addNode(label);
             node = findNode(label);
-        } return;
+        }
+
+        return node;
     }
 
     public void addEdge(Label l1, Label l2) {
-        GraphNode node1 = findNode(l1);
-        GraphNode node2 = findNode(l2);
-        if (node1 != null && node2 != null) {
-            node1.edges.add(node2);
-            node2.edges.add(node1);
-        }
+        GraphNode node1 = getOrCreateNode(l1);
+        GraphNode node2 = getOrCreateNode(l2);
+
+        node1.edges.add(node2);
+        node2.edges.add(node1);
     }
 
-    public void print() {
-        GraphNode<Label> node = headNode;
-        while (node != null) {
-            System.out.print("[ " + node.id + " : ");
-            for (GraphNode<Label> neighbor : node.edges) {
-                System.out.print(neighbor.id + " ");
-            }
-            System.out.println("]");
-            node = node.next;
-        }
-    }
+}
 
-    public static void main(String[] args) {
-
-    }
+public static void main(String[] args) {
 
 }
