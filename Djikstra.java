9657343d1bac54eb6a34bb84466c262f6d866171
SirPoppyThFatCat <samuel.demarchi07@gmail.com>
2026-05-17 22:09:36 +0100

    yk what I'm gonna go and have a break. my brain hurts

--- Djikstra.java
+++ Djikstra.java
@@ -1,5 +1,5 @@
 public class Djikstra {
     public static void main(String[] args) {
-        System.out.println("Djikstra's algorithm is one of the algorithms of all tim");
+        System.out.println("Djikstra's algorithm is one of the algorithms of all tim"); //yes
     }
 }
